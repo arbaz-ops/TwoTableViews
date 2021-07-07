d54f679cad51ec6053debdebd4674ab99cfefbc7
arbaz-ops <arbazchughtai55@gmail.com>
2021-07-07 18:15:36 +0500

    Initial Commit

---
+++ TwoTableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoTableViews
+//
+//  Created by Mac on 07/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
